a6df9fa958719c73da7495daaae195c19fe4d2fc
dcnis <bgk.dennis@yahoo.de>
2022-01-08 11:49:40 +0100

    removed endpoint createDB

--- src/main/java/de/schmidtdennis/mysqlspring/api/UserController.java
+++ src/main/java/de/schmidtdennis/mysqlspring/api/UserController.java
@@ -1,6 +1,5 @@
 package de.schmidtdennis.mysqlspring.api;
 
-import de.schmidtdennis.mysqlspring.mapper.DatabaseMapper;
 import de.schmidtdennis.mysqlspring.mapper.UserMapper;
 import de.schmidtdennis.mysqlspring.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +13,6 @@ public class UserController {
     @Autowired
     private UserMapper userMapper;
 
-    @Autowired
-    private DatabaseMapper databaseMapper;
-
-
     @PostMapping(value = "user/add")
     public String addUser(@RequestBody User user){
         userMapper.addUser(user.getFirstName(), user.getLastName(), user.getEmail());
@@ -27,11 +22,6 @@ public class UserController {
     @GetMapping("user/getAll")
     public List<User> getAll(){
         return userMapper.getAllUser();
-    }
-
-    @PostMapping("/createTable")
-    public void createTable(@RequestBody String dbName){
-        databaseMapper.createTableIfNotExists(dbName);
     }
 
 }

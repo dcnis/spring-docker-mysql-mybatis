86935dbb878e5b30f3246286b74cf80d4541de4e
dcnis <bgk.dennis@yahoo.de>
2022-01-12 14:23:22 +0100

    removed Student

--- src/main/java/de/schmidtdennis/mysqlspring/enums/Gender.java
+++
@@ -1,5 +0,0 @@
-package de.schmidtdennis.mysqlspring.enums;
-
-public enum Gender {
-    MALE, FEMALE
-}
--- src/main/java/de/schmidtdennis/mysqlspring/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package de.schmidtdennis.mysqlspring.repository;
-
-import de.schmidtdennis.mysqlspring.model.Student;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends CrudRepository<Student, String> {
-}

587961e22009bbf3d8e5f00b8f2494e634851727
dcnis <bgk.dennis@yahoo.de>
2022-01-28 14:20:01 +0100

    loading lesson and audio from docker instead of heroku

--- frontend/src/config/constants.js
+++ frontend/src/config/constants.js
@@ -4,7 +4,8 @@ export default {
     ADD_LATEST_LESSON_OF_USER: 'https://heroku-popup-chinese-backend.herokuapp.com/addLatestLessonsOfUser',
     UPDATE_USER_LESSON: 'https://heroku-popup-chinese-backend.herokuapp.com/updateUserLesson',
     FIND_LESSONS_BY_DIFFICULTY: 'https://heroku-popup-chinese-backend.herokuapp.com/findLessonsByDifficulty',
-    GET_LESSON: 'https://heroku-popup-chinese-backend.herokuapp.com/getLesson/',
+    GET_LESSON: 'http://localhost:3000/lesson/',
+    GET_AUDIO: 'http://localhost:3000/audio/',
     GET_SINGLE_USER_LESSON: 'https://heroku-popup-chinese-backend.herokuapp.com/getSingleUserLesson/',
     SEARCH_LESSON: 'https://heroku-popup-chinese-backend.herokuapp.com/searchLesson'
   }
